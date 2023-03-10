5e48e49005a752fd2d76b2c6806d8c3c7a2a8559
Quentin Bonnet <quentin.bonnet.work@gmail.com>
2023-03-10 01:37:09 +0100

    Remove unused private method in Config class.

--- src/main/java/lequentin/cocobot/config/Config.java
+++ src/main/java/lequentin/cocobot/config/Config.java
@@ -40,20 +40,4 @@ public class Config {
     public Secrets getSecrets() {
         return secrets;
     }
-
-    // get a file from the resources folder
-    // works everywhere, IDEA, unit test and JAR file.
-    private InputStream getFileFromResourceAsStream(String fileName) {
-
-        // The class loader that loaded the class
-        ClassLoader classLoader = getClass().getClassLoader();
-        InputStream inputStream = classLoader.getResourceAsStream(fileName);
-
-        // the stream holding the file content
-        if (inputStream == null) {
-            throw new IllegalArgumentException("file not found! " + fileName);
-        } else {
-            return inputStream;
-        }
-    }
 }
