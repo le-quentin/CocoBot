0d44fd24c65c6170ecd2b0bdbf32dc961b3a81b0
Quentin Bonnet <quentin.bonnet.work@gmail.com>
2024-01-25 22:03:57 +0100

    Add debug logging around the buggy part

--- src/main/java/lequentin/cocobot/discord/DiscordIncomingMessage.java
+++ src/main/java/lequentin/cocobot/discord/DiscordIncomingMessage.java
@@ -4,10 +4,16 @@ import discord4j.core.spec.MessageCreateSpec;
 import lequentin.cocobot.application.BotMessage;
 import lequentin.cocobot.application.IncomingMessage;
 import lequentin.cocobot.domain.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
+import java.util.logging.Level;
+
 public class DiscordIncomingMessage implements IncomingMessage {
+
+    private static Logger log = LoggerFactory.getLogger(DiscordIncomingMessage.class);
 
     private final discord4j.core.object.entity.Message discordMessage;
     private final DiscordConverter converter;
@@ -24,10 +30,14 @@ public class DiscordIncomingMessage implements IncomingMessage {
 
     @Override
     public Mono<Message> reply(BotMessage message) {
+        log.debug("Replying with message: {}", message.getText());
         Mono<Message> messageMono = discordMessage.getChannel()
+                .log("lequentin.cocobot.discord.DiscordIncomingMessage-channel", Level.FINE)
                 .subscribeOn(Schedulers.immediate())
                 .map(channel -> channel.createMessage(MessageCreateSpec.builder().content(message.getText()).build()))
-                .flatMap(messageCreateSpec -> messageCreateSpec.map(converter::toDomain));
+                .log("lequentin.cocobot.discord.DiscordIncomingMessage-message", Level.FINE)
+                .flatMap(messageCreateSpec -> messageCreateSpec.map(converter::toDomain))
+                .log("lequentin.cocobot.discord.DiscordIncomingMessage-converted", Level.FINE);
         messageMono.subscribe();
         return messageMono;
     }
